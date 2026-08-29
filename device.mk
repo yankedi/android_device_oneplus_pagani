877593fbad05b453fca8dfd0a9e59daad6697f32
yankedi <2696596951@qq.com>
2026-08-29 20:59:50 +0800

    pagani: Enable QTI OPlus UDFPS support

--- device.mk
+++ device.mk
@@ -23,6 +23,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
+$(call soong_config_set_bool,qtidisplay,oplus_udfps,true)
 
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
